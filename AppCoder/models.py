1e9dbcb9bded9b9c33a56a3dd291ca4a9b2e4feb
FrancoDiMartino <francogdimartino@gmail.com>
2022-11-22 23:11:36 -0300

    fin clase 20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -7,7 +7,7 @@ class Curso(models.Model):
     comision=models.IntegerField()
 
     def __str__(self):
-        return self.nombre+" "+self.comision
+        return self.nombre+" "+str(self.comision)
 
 
 
